7604b0bbd3ffda9d1a4f990b14497a1799b991ef
Jamie Lin <jamie.miki.lin@gmail.com>
2024-03-18 18:27:24 -0400

    add app use express.json

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 const app = express();
+app.use(express.json());
 Lab5(app);
 Hello(app);
 app.listen(4000);
