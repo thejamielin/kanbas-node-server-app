bf4a892e5edecc9cecadddbb9d5e6b567617c8c5
Jamie Lin <jamie.miki.lin@gmail.com>
2024-03-26 16:57:49 -0400

    add route to create a new module

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,5 +1,15 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
+  app.post("/api/courses/:cid/modules", (req, res) => {
+    const { cid } = req.params;
+    const newModule = {
+      ...req.body,
+      course: cid,
+      _id: new Date().getTime().toString(),
+    };
+    db.modules.push(newModule);
+    res.send(newModule);
+  });
   app.get("/api/courses/:cid/modules", (req, res) => {
     const { cid } = req.params;
     const modules = db.modules.filter((m) => m.course === cid);
