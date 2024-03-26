f8c7275f3d5a5d2a48cb2bc5541687aeb61880dc
Jamie Lin <jamie.miki.lin@gmail.com>
2024-03-26 17:06:46 -0400

    add put and delete routes for modules

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,5 +1,19 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex((m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body,
+    };
+    res.sendStatus(204);
+  });
+  app.delete("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    db.modules = db.modules.filter((m) => m._id !== mid);
+    res.sendStatus(200);
+  });
   app.post("/api/courses/:cid/modules", (req, res) => {
     const { cid } = req.params;
     const newModule = {
