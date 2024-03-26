9ae43cf1b4bfcf7aa2b3e4ed1f13801293026852
Jamie Lin <jamie.miki.lin@gmail.com>
2024-03-26 15:45:16 -0400

    add retrieving courses functionality for a5

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
